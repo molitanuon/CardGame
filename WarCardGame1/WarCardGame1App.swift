fdfed663e68ddd560dbdb803ca9d7cba76b4bd7a
Molita <molitanuon36@gmail.com>
2021-07-28 19:11:19 -0700

    Initial Commit

---
+++ WarCardGame1/WarCardGame1App.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGame1App.swift
+//  WarCardGame1
+//
+//  Created by Molita Nuon on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGame1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
